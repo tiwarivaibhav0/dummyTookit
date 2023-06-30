04146d3c7e83fcbf3efb5d9b46b4cf730a1e8925
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2023-06-30 15:20:27 +0530

    serializable warning removed

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./UserSlice";
 import storage from "redux-persist/lib/storage";
-import { persistReducer } from "redux-persist";
-
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist";
 const pConfig={
   key:"root",
   version:1,
@@ -12,4 +19,10 @@ export const store = configureStore({
   reducer: {
     usersReducer:persistReducer(pConfig,usersReducer),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
